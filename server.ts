2bfd41de5a9c8bb0abb32b43c424f3bb5c17864f
Phạm Ngọc Quốc Khánh <phamngocquockhanh2004@gmail.com>
2025-05-08 23:20:59 +0700

    add line with log

--- server.ts
+++ server.ts
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   const ip = req.headers["x-forwarded-for"] || req.ip;
   const path = req.path;
-  console.log();
+  console.log("\n====== INCOMING REQUEST ======");
   console.log(`Request from ${ip} to ${path}`);
   next();
 });
