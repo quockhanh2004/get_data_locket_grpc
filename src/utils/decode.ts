ab67354ea1d2be1b5fef38691df96752e3345604
Phạm Ngọc Quốc Khánh <phamngocquockhanh2004@gmail.com>
2025-05-08 23:08:36 +0700

    get user id

--- src/utils/decode.ts
+++ src/utils/decode.ts
@@ -4,6 +4,7 @@ export function decodeJwt(token: string): Record<string, any> | null {
 
     const padded = payloadBase64.replace(/-/g, "+").replace(/_/g, "/");
     const jsonStr = decodeURIComponent(escape(atob(padded))); // <-- fix unicode
+    console.log("userId", JSON.parse(jsonStr)?.user_id);
 
     return JSON.parse(jsonStr);
   } catch (e) {
